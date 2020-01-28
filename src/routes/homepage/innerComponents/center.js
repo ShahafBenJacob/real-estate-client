15df69e7e8a3fd89c527922f3d6c2f0efe2acfd1
shaha <shahafbenjakov@gmail.com>
2020-01-28 10:03:33 +0200

    create route and link to each apartment

--- src/routes/homepage/innerComponents/center.js
+++ src/routes/homepage/innerComponents/center.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-function CenterPage(props) {
+function CenterPage() {
   return (
     <div
       className={"background-image"}
